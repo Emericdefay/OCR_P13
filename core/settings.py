5b8b7942a593d6018a63e2c76773887aa1a6712a
emericdefay <emericdefay@gmail.com>
2021-06-29 12:49:45 +0200

    Issue #26 : Set a Sentry project done

--- core/settings.py
+++ core/settings.py
@@ -111,3 +111,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Sentry integration 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://4f10489e295f4b8691dd0dbd300a74b7@o889839.ingest.sentry.io/5838936",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -5,9 +5,15 @@ from django.urls import path, include
 from . import views
 
 
+def trigger_error(request):
+    """Just there to show error events on sentry"""
+    division_by_zero = 1 / 0
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include('lettings.urls', namespace='lettings')),
     path('profiles/', include('profiles.urls', namespace='profiles')),
     path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error),  # Only there to show error events
 ]
+
